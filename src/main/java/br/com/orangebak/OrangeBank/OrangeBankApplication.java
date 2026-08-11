37e722e616a4ba8c8ea0b22ff69ee729371394f4
Hiago 7k <hiago7k@gmail.com>
2026-08-11 20:52:33 -0300

    starting project

---
+++ src/main/java/br/com/orangebak/OrangeBank/OrangeBankApplication.java
@@ -0,0 +1,13 @@
+package br.com.orangebak.OrangeBank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrangeBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrangeBankApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/orangebak/OrangeBank/OrangeBankApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.orangebak.OrangeBank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrangeBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
