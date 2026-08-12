3013a995fe199e728aacbd61488880f9c240bc35
Hiago 7k <hiago7k@gmail.com>
2026-08-11 21:15:22 -0300

    low validation

--- src/main/java/br/com/orangebak/OrangeBank/OrangeBankApplication.java
+++ src/main/java/br/com/orangebak/OrangeBank/OrangeBankApplication.java
@@ -4,6 +4,8 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Scanner;
+
 @SpringBootApplication
 public class OrangeBankApplication implements CommandLineRunner {
 
@@ -16,6 +18,11 @@ public class OrangeBankApplication implements CommandLineRunner {
 		System.out.println("********************************");
 		System.out.println("---- Welcome to Orange Bank ----");
 		System.out.println("********************************");
+		Scanner keyboard = new Scanner(System.in);
+
+		System.out.println("Type your option: ");
+		System.out.println("1. Login");
+		System.out.println("2. Create a new Account");
 
 
 
