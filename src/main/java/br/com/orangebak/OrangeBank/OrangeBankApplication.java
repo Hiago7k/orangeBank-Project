f01dd641a059c3260ee44b8343069887f33d7373
Hiago 7k <hiago7k@gmail.com>
2026-08-11 21:10:21 -0300

    structre initia;

--- src/main/java/br/com/orangebak/OrangeBank/OrangeBankApplication.java
+++ src/main/java/br/com/orangebak/OrangeBank/OrangeBankApplication.java
@@ -1,13 +1,23 @@
 package br.com.orangebak.OrangeBank;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class OrangeBankApplication {
+public class OrangeBankApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(OrangeBankApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("********************************");
+		System.out.println("---- Welcome to Orange Bank ----");
+		System.out.println("********************************");
+
+
+
+	}
 }
